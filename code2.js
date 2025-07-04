d665dd37e5aad0533646d9405ed42e65d061c1f3
TikshanGaba <25371tg@hvhs.school.nz>
2025-07-04 07:10:30 +0000

    Jul 4, 2025, 7:10 AM

---
+++ code2.js
@@ -0,0 +1,15 @@
+let userName;
+let userAge;
+let userMoney;
+let MINAGE = 18;
+let MAXAGE = 100;
+let cars = ["BMW", "Camry", "Civic"]
+let seats = ["2","4","5"];
+let choice = prompt("Pick how much seats you want in your car");
+function start()
+{
+    <input type="text" id="input_name" required></input>
+}
+{
+    <input type="number" id="input_age" min={18} max={100} required></input>
+}

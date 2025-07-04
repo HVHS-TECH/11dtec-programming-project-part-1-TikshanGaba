9a5e9a56f5af77e8bb4af3e7d2b8b8b2881ca895
TikshanGaba <25371tg@hvhs.school.nz>
2025-07-04 06:51:54 +0000

    Jul 4, 2025, 6:51 AM

--- code.js
+++ code.js
@@ -27,7 +27,7 @@ function start()
 }
  alert("Okay " + username + " let's start your process")
  alert("Choose the car you want according to your seats. We have 3 options of seats, 2 seats, 4 seats, 5 seats")
- if 
+ if seats = 2 
  
 
       }

87b1efd84d966cc3e4bdb128a4ec6f8f3f803e13
TikshanGaba <25371tg@hvhs.school.nz>
2025-07-04 11:44:13 +0000

    BAKA

--- code.js
+++ code.js
@@ -3,12 +3,10 @@ let userAge;
 let userMoney;
 let MINAGE = 18;
 let MAXAGE = 100;
-let cars = ["BMW", "Camry", "Civic"]
-let seats = ["2","4","5"];
-let choice = prompt("Pick how much seats you want in your car");
+let cars = ["Camry","BMW","Focus","F-150"];
 function start()
  {
-    userName = prompt("Welcome to Rob's rentals. Can I know your name ?")
+    userName = prompt("Welcome to QuickRide rentals. Can I know your name ?")
     if ( !isNaN(userName) || userName == null || userName == "" || userName == " " ){
     userName = prompt("Invalid! You must enter a name")
     // check if user has clicked cancel
@@ -26,9 +24,5 @@ function start()
    return;
  }
 }
- alert("Okay " + userName + " let's start your process")
- alert("Choose the car you want according to your seats. We have 3 options of seats, 2 seats, 4 seats, 5 seats")
-  
- 
-
+alert("There are 4 cars you can rent : " + cars)
  }
